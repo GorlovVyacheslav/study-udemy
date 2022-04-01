fd39a32adf5481cdf0ad94687b81da74679de017
vyacheslav <somebody@gmail.com>
2022-04-01 14:40:31 +0200

    continue example

--- lesson01/src/main.js
+++ lesson01/src/main.js
@@ -1,15 +1,31 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
 };
-const a = prompt('Один из последних просмотренных фильмов?'),
-  b = prompt('На сколько оцените его?'),
-  c = prompt('Один из последних просмотренных фильмов?'),
-d = prompt('На сколько оцените его?');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('normas');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+if (personalMovieDB.count < 10) {
+  console.log('Prosmotreno malo filmov');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('you classical zritel');
+} else if (personalMovieDB.count >= 30) {
+  console.log('you kinoman');
+} else {
+  console.log('this error');
+}
 console.log(personalMovieDB);
